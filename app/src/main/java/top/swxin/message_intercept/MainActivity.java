e2b7be33071dacd0d87c1c7bcbd2041404ecd3b3
swxin <2831207404@qq.com>
2022-02-25 23:00:17 +0800

    Initial commit

---
+++ app/src/main/java/top/swxin/message_intercept/MainActivity.java
@@ -0,0 +1,14 @@
+package top.swxin.message_intercept;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
